8dbc887d1682373fce0d634c4b93e02da435c29f
nb9309 <108428369+nb9309@users.noreply.github.com>
2024-09-02 23:29:50 +0530

    Add files via upload

---
+++ assignment operator.py
@@ -0,0 +1,5 @@
+a = input('enter value:')
+b = input('enter value:') #single assignment operator
+a,b = b,a                 #multi assignment operator
+print('value of a is %s' %a)
+print('value of b is %s' %b)
---
+++ multi assignment operator.py
@@ -0,0 +1,13 @@
+a = float(input('enter value:'))
+b = float(input('enter value:'))
+add,sub,mul,div,mod,floor,exponen = a+b,a-b,a*b,a/b,a%b,a//b,a**b
+#multi line assignment
+print('-'*50)
+print('sum of a,b is %0.2f' %add)
+print('sub of a,b is %0.2f' %sub)
+print('mul of a,b is %0.2f' %mul)
+print('div of a,b is %0.2f' %div)
+print('mod of a,b is %0.2f' %mod)
+print('floor of a,b is %0.2f' %floor)
+print('exponen of a,b is %0.2f' %exponen)
+print('-'*50)
